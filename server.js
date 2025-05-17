8e62f37d0051d36eab4a7ca5a7a9bd74432d6a5e
ehsonidizoda <164121256+ehsonidizoda@users.noreply.github.com>
2025-05-17 09:04:22 +0500

    Update server.js

--- server.js
+++ server.js
@@ -73,3 +73,13 @@ app.get('/admin/generate-code', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
+
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('path/to/key.pem'),
+  cert: fs.readFileSync('path/to/cert.pem')
+};
+
+https.createServer(options, app).listen(443);
